a3c22347aaff612f1f8210ca68a5fcdc0e80066a
EskildAndersen <202007483@post.au.dk>
2022-02-27 22:16:59 +0100

    lidt mere

--- python/DICE.py
+++ python/DICE.py
@@ -7,40 +7,60 @@ File name: DICE.py
 
 Discription:
 
-Python script to calculate the DICE coefficient between two surfaces. 
+Python script to calculate the DICE coefficient between the same set of labels of pixels of two images P1, P2. Background is assumed to be 0.
 """
 
 # Imports
 import os
 import SimpleITK as ITK
 import numpy as np
+import DataReader
+import DataPreparation
 
 # Import other files
 
 
 # Classes and functions
 def DICE(P1, P2):
-    assert isinstance(P1, )
-    assert isinstance(P2, )
-    
+
+    #Input must be of type ITK.Image:
+    assert isinstance(P1, ITK.SimpleITK.Image)
+    assert isinstance(P2, ITK.SimpleITK.Image)
+
+
+    #Save results in list and append to dictionary
     Dice_list = list()
     Jaccard_list = list()  
     quality = dict()
 
-    labelPred = ITK.GetArrayFromImage(P1, isVector=False)
-    labelTrue = ITK.GetArrayFromImage(P2, isVector=False)
 
+    #Compute overlap measurements:
     dicecomputer = ITK.LabelOverlapMeasuresImageFilter()
-    dicecomputer.Execute(labelTrue>0.5,labelPred>0.5)
+    dicecomputer.Execute(P2>0.5,P1>0.5)
     quality["dice"]=dicecomputer.GetDiceCoefficient()
     quality["jaccard"]=dicecomputer.GetJaccardCoefficient()
 
     Dice_list.append(quality["dice"])
-    Jaccard_list.append(quality["jaccard"]
+    Jaccard_list.append(quality["jaccard"])
 
     return quality
 
 # Run file (optional)
+
+ID = "1cbDrFdyzAXjFICMJ58Hmja9U"     
+
+x = DataReader.datareader(ID)
+P1 = x.DL
+P2 = x.GT
+segment = 'BraiNSteM'
+
+P1 = DataPreparation.OAR(P1, segment)
+P2 = DataPreparation.OAR(P2, segment)
+
+P1 = P1.GetImage()
+P2 = P2.GetImage()
+
+DICE(P1, P2)
 
 
 
--- python/Hausdorff.py
+++ python/Hausdorff.py
@@ -7,30 +7,36 @@ File name: Hausdorff.py
 
 Discription:
 
-Python script to calculate the Hausdorff Distance between two surfaces. 
+Python script to calculate the Hausdorff Distance between the same set of labels consisting of pixels from two images P1, P2. Background is assumed to be 0 
 """
 
 # Imports
 import os
 import SimpleITK as ITK
 import numpy as np
+import DataReader
+import DataPreparation
 
 # Import other files
 
 
 # Classes and functions
 def Hausdorff(P1, P2):
-    assert isinstance(P1, )
-    assert isinstance(P2, )
 
+    #Input must be of type ITK.Image:
+    assert isinstance(P1, ITK.SimpleITK.Image)
+    assert isinstance(P2, ITK.SimpleITK.Image)
+
+
+    #Save results in list and append to dictionary
     AvgHausdorff_list = list()
     Hausdorff_list = list()
+    quality = dict()
 
-    labelPred = ITK.GetImageFromArray(P1, isVector=False)
-    labelTrue = ITK.GetImageFromArray(P2, isVector=False)
-    
+
+    #Compute overlap measurements:
     hausdorffcomputer = ITK.HausdorffDistanceImageFilter()
-    hausdorffcomputer.Execute(labelTrue>0.5,labelPred>0.5)
+    hausdorffcomputer.Execute(P2>0.5,P1>0.5)
 
     quality["avgHausdorff"]=hausdorffcomputer.GetAverageHausdorffDistance()
     quality["Hausdorff"]=hausdorffcomputer.GetHausdorffDistance()
@@ -42,5 +48,18 @@ def Hausdorff(P1, P2):
 
 # Run file (optional)
 
+ID = "1cbDrFdyzAXjFICMJ58Hmja9U"     
 
+x = DataReader.datareader(ID)
+P1 = x.DL
+P2 = x.GT
+segment = 'BraiNSteM'
+
+P1 = DataPreparation.OAR(P1, segment)
+P2 = DataPreparation.OAR(P2, segment)
+
+P1 = P1.GetImage()
+P2 = P2.GetImage()
+
+Hausdorff(P1, P2)
 
