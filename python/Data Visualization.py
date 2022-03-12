d58363bf4553d3cf3cba8b5c5eb28edbcf09c0a3
Eskild Andersen <202007483@post.au.dk>
2022-03-12 20:32:38 +0100

    Create Data Visualization.py

---
+++ python/Data Visualization.py
@@ -0,0 +1,14 @@
+'''
+Data visualization
+
+'''
+
+# Imports
+import os
+import SimpleITK as ITK
+from DataReader import Path
+from DataPreparation import OAR_Image
+from eskildmetric import Metrics
+
+
+
