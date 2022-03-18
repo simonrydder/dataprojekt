89beb8f42b171c469f2b8f0f7a75e28826a995e9
Simon Rydder <simonrydder@gmail.com>
2022-03-18 10:23:17 +0100

    PEP8 changes to ALP and new findLines method added

--- python/APL.py
+++ python/APL.py
@@ -20,10 +20,10 @@ import DataPreparation
 
 
 #Classes and functions
-class Addedpathlength():
-    def __init__(self,img1,img2):
-        self.width,self.height, _ = img1.Spacing
-        _,_, self.z = img1.Shape
+class AddedPathLength():
+    def __init__(self, img1, img2):
+        self.width, self.height, _ = img1.Spacing   # x, y, z
+        _, _, self.z = img1.Shape   # x, y, z
         self.A = img1.GetArray()
         self.B = img2.GetArray()
         self.APL = self.getAPL()
@@ -35,72 +35,89 @@ class Addedpathlength():
         self.True_Length = 0
         self.True_Area = 0
         for i in range(self.z):
-            coordsA = self.findCoordinates(self.A[i])
-            coordsB = self.findCoordinates(self.B[i])
-            VA, HA = self.findLines(coordsA)
-            VB, HB = self.findLines(coordsB)
+            coordsA = self.findCoordinates(self.A[i])   # Coordinates for Image A
+            coordsB = self.findCoordinates(self.B[i])   # Coordinates for Image B
+            VA, HA = self.findLines(coordsA)    # Sets of Vertical and Horizontal Lines for A
+            VB, HB = self.findLines(coordsB)    # Sets of Vertical and Horizontal Lines for B
 
-
-            VA = set(VA)
-            HA = set(HA)
-            VB = set(VB)
-            HB = set(HB)
             vrest = VA - VB
             hrest = HA - HB
 
-            # vrest = (set(VA) - set(VB))
-            # hrest = (set(HA) - set(HB))
-
-            self.True_Length += len(VA)*self.height+len(HA)*self.width
-            self.True_Area += len(coordsA)*self.height+len(coordsA)*self.width
+            self.True_Length += (len(VA) * self.height) + (len(HA) * self.width)
+            self.True_Area += (self.height + self.width) * len(coordsA)     # For area of A
+            # self.True_Area += (self.height + self.width) * len(coordsA & coordsB)     # For area of the intersection of A and B
 
             APL_total += len(vrest) * self.height + len(hrest) * self.width
 
         return APL_total
 
     def getLineRatio(self):
-        return self.APL/self.True_Length
+        return self.APL / self.True_Length
 
     def getAPLvolume(self):
-        return self.APL/self.True_Area
+        return self.APL / self.True_Area
 
-    def findCoordinates(self,array):
+    def findCoordinates(self, array):
         """
         Input:  An array A[y][x]
         Output: A list of coordinates of the form (x + 1, len(A) - y), where A[y][x] == 1.
         """
         N = len(array)
         coords = np.where(array > 0)
-        return [(x + 1, N - y) for y, x in zip(*coords)]
+        return {(x + 1, N - y) for y, x in zip(*coords)}
 
-    def findLines(self,coordinates):
+    # def findLines(self, coordinates):
+    #     """
+    #     Input:  A list of coordinates for the center of a pixel.
+    #     Outout: Two lists of linesegments. One for horizontal linesegments and one for vertical linesegments.
+    #     """
+
+    #     verticals = {}
+    #     horizontals = {}
+
+    #     for x, y in coordinates:
+    #         left  = ((x - 0.5, y - 0.5), (x - 0.5, y + 0.5)) # left edge line
+    #         right = ((x + 0.5, y - 0.5), (x + 0.5, y + 0.5)) # right edge line
+    #         lower = ((x - 0.5, y - 0.5), (x + 0.5, y - 0.5)) # lower edge line
+    #         upper = ((x - 0.5, y + 0.5), (x + 0.5, y + 0.5)) # upper edge line
+
+    #         for line in left, right, lower, upper:
+    #             (x0, _), (x1, _) = line
+    #             if x0 == x1: # vertical line
+    #                 verticals[line] = verticals.get(line, 0) + 1
+    #             else: # horizontal line
+    #                 horizontals[line] = horizontals.get(line, 0) + 1 
+        
+    #     # Extraxt only lines not dublicating.
+    #     Vlines = {key for key, value in verticals.items() if value == 1}
+    #     Hlines = {key for key, value in horizontals.items() if value == 1}
+        
+    #     return Vlines, Hlines
+
+    def findLines(coordinates):
         """
         Input:  A list of coordinates for the center of a pixel.
         Outout: Two lists of linesegments. One for horizontal linesegments and one for vertical linesegments.
         """
 
-        verticals = {}
-        horizontals = {}
+        vLines = set()
+        hLines = set()
 
         for x, y in coordinates:
-            left = ((x - 0.5, y - 0.5), (x - 0.5, y + 0.5)) # left edge line
+            left  = ((x - 0.5, y - 0.5), (x - 0.5, y + 0.5)) # left edge line
             right = ((x + 0.5, y - 0.5), (x + 0.5, y + 0.5)) # right edge line
             lower = ((x - 0.5, y - 0.5), (x + 0.5, y - 0.5)) # lower edge line
             upper = ((x - 0.5, y + 0.5), (x + 0.5, y + 0.5)) # upper edge line
 
-            for line in left, right, lower, upper:
-                (x0, _), (x1, _) = line
-                if x0 == x1: # vertical line
-                    verticals[line] = verticals.get(line, 0) + 1
-                else: # horizontal line
-                    horizontals[line] = horizontals.get(line, 0) + 1 
-        
-        # Extraxt only lines not dublicating.
-        Vlines = [key for key, value in verticals.items() if value == 1]
-        Hlines = [key for key, value in horizontals.items() if value == 1]
-        
-        return Vlines, Hlines
+            # Removing vertical dublicates
+            vTemp = {left, right}
+            vLines = (vLines | vTemp) - (vLines & vTemp)
 
+            # Removeing horizontal dublicates
+            hTemp = {upper, lower}
+            hLines = (hLines | hTemp) - (hLines & hTemp)
+        
+        return vLines, hLines
 
 # Test
 
@@ -113,7 +130,7 @@ OAR_DL = DataPreparation.OAR_Image(fileDL,segment)
 OAR_GT = DataPreparation.OAR_Image(fileGT,segment)
 
 
-test = Addedpathlength(OAR_GT,OAR_DL)
+test = AddedPathLength(OAR_GT,OAR_DL)
 
 test.APL
 test.APL_line_ratio
